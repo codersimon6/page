c9528bbd56f0d37e654088a894260cfa851f233d
codersimon6 <2535342895@qq.com>
2023-05-22 17:16:03 +0800

    1

---
+++ components/MyFooter/index.jsx
@@ -0,0 +1,21 @@
+const MyFooter = () => {
+    return (
+        <>
+            {/*<Button*/}
+            {/*    type="link"*/}
+            {/*    href="https://twitter.com/jingluo0"*/}
+            {/*    target="_blank"*/}
+            {/*    icon={<TwitterOutlined/>}*/}
+            {/*    size={"middle"}*/}
+            {/*/>*/}
+            {/*<Button*/}
+            {/*    type="link"*/}
+            {/*    href="https://github.com/wxtsky/MyWalletScan"*/}
+            {/*    target="_blank"*/}
+            {/*    icon={<GithubOutlined/>}*/}
+            {/*    size={"middle"}*/}
+            {/*/>*/}
+        </>
+    )
+}
+export default MyFooter
